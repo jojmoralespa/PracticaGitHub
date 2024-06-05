1e40c4be607a9f4ec0f0ece1fba09f4d9d2649a6
DaniloHernández <111465074+DaniloHernandezM@users.noreply.github.com>
2024-06-05 18:42:27 -0500

    Importación de archivos VPC y Base de datos RDS

---
+++ rds.tf
@@ -0,0 +1,19 @@
+resource "aws_db_instance" "default" {
+  identifier             = "reto-rds"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "8.0.36"
+  instance_class         = "db.t3.micro"
+  db_name                = "mydb"
+  username               = "admin"
+  password               = "password"
+  parameter_group_name   = "default.mysql8.0"
+  db_subnet_group_name   = aws_db_subnet_group.db.name
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+
+  tags = {
+    Name = "reto-rds"
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,83 @@
+resource "aws_vpc" "vpc_reto" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc_reto"
+  }
+}
+
+resource "aws_subnet" "subnet1_vpc_reto" {
+  vpc_id            = aws_vpc.vpc_reto.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "subnet1_vpc_reto"
+  }
+}
+
+resource "aws_subnet" "subnet2_vpc_reto" {
+  vpc_id            = aws_vpc.vpc_reto.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "subnet2_vpc_reto"
+  }
+}
+
+resource "aws_subnet" "subnet3_vpc_reto" {
+  vpc_id            = aws_vpc.vpc_reto.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "subnet3_vpc_reto"
+  }
+}
+
+resource "aws_db_subnet_group" "db" {
+  name       = "db-subnet-group"
+  subnet_ids = [aws_subnet.subnet1_vpc_reto.id, aws_subnet.subnet2_vpc_reto.id, aws_subnet.subnet3_vpc_reto.id]
+
+  tags = {
+    Name = "db-subnet-group"
+  }
+}
+
+resource "aws_internet_gateway" "gw_reto" {
+  vpc_id = aws_vpc.vpc_reto.id
+
+  tags = {
+    Name = "gw_reto"
+  }
+}
+
+resource "aws_route_table" "rt_reto" {
+  vpc_id = aws_vpc.vpc_reto.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_reto.id
+  }
+
+  tags = {
+    Name = "rt_reto"
+  }
+}
+
+resource "aws_route_table_association" "a_rt_reto_subnet1" {
+  subnet_id      = aws_subnet.subnet1_vpc_reto.id
+  route_table_id = aws_route_table.rt_reto.id
+}
+
+resource "aws_route_table_association" "a_rt_reto_subnet2" {
+  subnet_id      = aws_subnet.subnet2_vpc_reto.id
+  route_table_id = aws_route_table.rt_reto.id
+}
+
+resource "aws_route_table_association" "a_rt_reto_subnet3" {
+  subnet_id      = aws_subnet.subnet3_vpc_reto.id
+  route_table_id = aws_route_table.rt_reto.id
+}
